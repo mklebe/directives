eadc68a99b7f6c1d0992532132fe29f5b20547ae
Matthias Klebe <matthiasklebe@gmail.com>
2016-10-26 19:38:17 +0200

    First Teasersample

--- app/scripts/directives/teaser.js
+++ app/scripts/directives/teaser.js
@@ -6,13 +6,33 @@
  * @description
  * # teaser
  */
+
+
 angular.module('directivesApp')
-  .directive('teaser', function () {
+  .directive('teaser', function ( ) {
+
     return {
-      template: '<div></div>',
+      templateUrl: function( ) {
+        //console.log( attr );
+        return 'scripts/directives/teaser.html';
+      },
       restrict: 'E',
-      link: function postLink(scope, element, attrs) {
-        element.text('this is the teaser directive');
+      transclude: true,
+      scope: {},
+      //compile: teaserCompile,
+      // adding content to the view
+      link: function( scope, element, attrs, controller, transclusionFn ) {
+        console.log( element );
+        console.log( controller );
+        console.log(attrs);
+        if( attrs.fontSize === 'extra-large' ) {
+          console.log('hero detected');
+          element.addClass('teaser--hero');
+        }
+        transclusionFn( scope, function( clone ) {
+          scope.headline = clone[1].innerText;
+          scope.bodytext = clone[3].innerText;
+        } );
       }
     };
   });
